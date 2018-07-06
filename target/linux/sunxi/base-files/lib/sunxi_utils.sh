4fbea638d859a5306b6e7554803898af73a08487
fengmushu <fengmushu@gmail.com>
2018-07-06 16:04:38 +0800

    fixup mac format init

--- target/linux/sunxi/base-files/lib/sunxi_utils.sh
+++ target/linux/sunxi/base-files/lib/sunxi_utils.sh
@@ -5,15 +5,6 @@ env_part="mmcblk2boot1"
 env_dev_path="/dev/$env_part"
 env_command="uboot-env -d $env_dev_path -o 0x0 -l 0x20000"
 
-env_reset_hw_addr() {
-
-    mac=`$env_command get ${env_iface}_mac 2>/dev/null | awk -F= '{print $2}'`
-
-    [ -z $mac ] || {
-        ifconfig ${env_iface} hw ether $mac 2>/dev/null
-    }
-}
-
 init_mac_random() {
 	echo 0 > /sys/block/$env_part/force_ro
 	$env_command get || {
@@ -26,13 +17,19 @@ init_mac_random() {
 	$env_command set ${env_iface}_mac $mac
 }
 
-fetch_mac_from_uboot_env() {
+env_reset_hw_addr() {
 	local mac
 
 	[ -b $env_dev_path ] || return
 
-	mac=`${env_command} get ${env_iface}_mac 2>/dev/null`
-	[ -z "$mac" ] && {
+    mac=`$env_command get ${env_iface}_mac 2>/dev/null | awk -F= '{print $2}'`
+
+    [ -z "$mac" ] && {
 		init_mac_random
+		mac=`$env_command get ${env_iface}_mac 2>/dev/null | awk -F= '{print $2}'`
 	}
+
+	[ -z "$mac" ] || {
+        ifconfig ${env_iface} hw ether $mac 2>/dev/null
+    }
 }
